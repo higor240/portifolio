dd6454745a32fe1c7a1e319a98d680328700104c
unknown <higorferreira240@gmail.com>
2024-09-06 14:59:29 -0300

    fix: Hero components Ui

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -11,10 +11,19 @@ const Hero = () => {
     height: "120vh",
     display: "flex",
     alignItems: "center",
+    [theme.breakpoints.up('xs')]:{
+      paddingTop: "100px"
+
+    },
+    [theme.breakpoints.up('md')]:{
+      paddingTop: "0"
+
+      
+    }
   }))
 
   const StyledImg = styled("img")(({ theme }) => ({
-    width: "80%",
+    width: "75%",
     borderRadius: "50%",
     border: `1px solid ${theme.palette.primary.contrastText}`
 
